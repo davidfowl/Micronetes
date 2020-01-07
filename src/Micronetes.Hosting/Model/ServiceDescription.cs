d7c83a2f98d3e66da8893284d91863eef67ab04f
David Fowler <davidfowl@gmail.com>
2020-01-06 16:57:24 -0800

    Added annotations

--- src/Micronetes.Hosting/Model/ServiceDescription.cs
+++ src/Micronetes.Hosting/Model/ServiceDescription.cs
@@ -8,6 +8,7 @@ namespace Micronetes.Hosting.Model
         public string Name { get; set; }
         public bool External { get; set; }
         public string DockerImage { get; set; }
+        public Dictionary<string, string> Annotations { get; set; } = new Dictionary<string, string>();
         public List<ServiceBinding> Bindings { get; set; } = new List<ServiceBinding>();
         public Dictionary<string, string> Configuration { get; set; } = new Dictionary<string, string>();
 
