8515168209c806b883db3ab4546bc8f923d92f97
David Fowler <davidfowl@gmail.com>
2020-01-21 00:20:32 -0800

    Small cleanup

--- src/Micronetes.Hosting/MicronetesHost.cs
+++ src/Micronetes.Hosting/MicronetesHost.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Micronetes.Hosting.Model;
 using Microsoft.AspNetCore.Builder;
